ebababddeb2d33a564d8a18a8f11625e596bc5fd
Ali Sadri <asadri721@gmail.com>
2021-10-26 12:11:13 +0330

    Create homescreen and product screen also Add react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,29 @@
-import Product from './components/Product';
-import data from './data';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div className='grid-container'>
-      <header className='row'>
-        <div>
-          <a className='brand' href='/'>
-            amazon
-          </a>
-        </div>
-        <div>
-          <a href='/cart'>Cart</a>
-          <a href='/signin'>Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div className='row center'>
-          {data.products.map((product) => (
-            <Product key={product._id} product={product} />
-          ))}
-        </div>
-      </main>
-      <footer className='row center'>All right reserved</footer>
-    </div>
+    <Router>
+      <div className='grid-container'>
+        <header className='row'>
+          <div>
+            <a className='brand' href='/'>
+              amazon
+            </a>
+          </div>
+          <div>
+            <a href='/cart'>Cart</a>
+            <a href='/signin'>Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path='/product/:id' component={ProductScreen}></Route>
+          <Route path='/' component={HomeScreen} exact></Route>
+        </main>
+        <footer className='row center'>All right reserved</footer>
+      </div>
+    </Router>
   );
 }
 
