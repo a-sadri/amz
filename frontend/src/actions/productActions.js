63a457bf1c4727a599e8e50edda46e1e5022f417
Ali Sadri <asadri721@gmail.com>
2021-10-27 15:14:21 +0330

    Implement redux in product screen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,5 +1,8 @@
 import Axios from 'axios';
 import {
+  PRODUCT_DETAILES_FAIL,
+  PRODUCT_DETAILES_REQUEST,
+  PRODUCT_DETAILES_SUCCESS,
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
@@ -14,5 +17,21 @@ export const listProducts = () => async (dispatch) => {
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
   } catch (error) {
     dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message });
+  }
+};
+
+export const detailsProduct = (productId) => async (dispatch) => {
+  dispatch({ type: PRODUCT_DETAILES_REQUEST, payload: productId });
+  try {
+    const { data } = await Axios.get(`/api/products/${productId}`);
+    dispatch({ type: PRODUCT_DETAILES_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILES_FAIL,
+      payload:
+        error.message && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
   }
 };
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,7 @@
 import {
+  PRODUCT_DETAILES_FAIL,
+  PRODUCT_DETAILES_REQUEST,
+  PRODUCT_DETAILES_SUCCESS,
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
@@ -14,6 +17,22 @@ export const productListReducer = (
     case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload };
     case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const productDetailReducer = (
+  state = { product: {}, loading: true },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_DETAILES_REQUEST:
+      return { loading: true };
+    case PRODUCT_DETAILES_SUCCESS:
+      return { loading: false, product: action.payload };
+    case PRODUCT_DETAILES_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
